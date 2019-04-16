2243324c66ce39e16764b2148517b9030b4c6faa
James Ware <jazz642@gmail.com>
2019-04-16 14:00:10 -0400

    corrected include and compiler path. Added include header for command argument library

--- HeadFirstC/Chapter3/categorize.c
+++ HeadFirstC/Chapter3/categorize.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 /* order of args will be the search word then the file output name.
 remember argv[0] is the name of the actual program getting ran 
 
