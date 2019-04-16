7ec34ecb0407a0662bd8947cb8ef0a697064a635
James Ware <jazz642@gmail.com>
2019-04-16 13:43:53 -0400

    added files to gitignore. Passed in command line arguments for file names. Put in some file existence validation.

--- HeadFirstC/Chapter3/categorize.c
+++ HeadFirstC/Chapter3/categorize.c
@@ -2,18 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+/* order of args will be the search word then the file output name.
+remember argv[0] is the name of the actual program getting ran 
+
+*/
+int main(int argc, char *argv[])
 {
     char line[80];
-    FILE *in = fopen("gpsdata.csv", "r");
-    FILE *file1 = fopen("ufos.csv", "w");
-    FILE *file2 = fopen("disappearances.csv", "w");
-    FILE *file3 = fopen("others.csv", "w");
+    if (argc != 6){
+        fprintf(stderr, "You need to give 5 arguments\n");
+        return 1;
+    }
+    //The example doesn't allow this to be a parameter, consider changing
+    FILE *in;
+    //fopen returns 0 if it cannot find the file, this ensures existence of file    
+    /*Consider putting checks like this in a function
+    * Also - check if STD library has a solution
+    */
+    if (!(in = fopen("gpsdata.csv", "r"))){
+        fprintf(stderr, "Can't open the file.\n");
+        //return a 1 for error and exit program
+        return 1;
+    }
+    FILE *file1 = fopen(argv[2], "w");
+    FILE *file2 = fopen(argv[4], "w");
+    FILE *file3 = fopen(argv[5], "w");
     while (fscanf(in, "%79[^\n]\n", line) == 1)
     {
-        if (strstr(line, "UFO"))
+        if (strstr(line, argv[1]))
             fprintf(file1, "%s\n", line);
-        else if (strstr(line, "Disappearance"))
+        else if (strstr(line, argv[3]))
             fprintf(file2, "%s\n", line);
         else
             fprintf(file3, "%s\n", line);
